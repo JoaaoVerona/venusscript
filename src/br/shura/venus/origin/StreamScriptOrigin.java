78de9a8a3ea53f5871d03c2faa34a0ecdd7cccb2
BloodShura <joaaoverona@gmail.com>
2016-05-11 00:46:53 -0300

    Added StreamScriptOrigin

---
+++ src/br/shura/venus/origin/StreamScriptOrigin.java
@@ -0,0 +1,66 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016, João Vitor Verona Biazibetti - All Rights Reserved                /
+//                                                                                       /
+// Licensed under the GNU General Public License v3;                                     /
+// you may not use this file except in compliance with the License.                      /
+//                                                                                       /
+// You may obtain a copy of the License at                                               /
+//     http://www.gnu.org/licenses/gpl.html                                              /
+//                                                                                       /
+// Unless required by applicable law or agreed to in writing, software                   /
+// distributed under the License is distributed on an "AS IS" BASIS,                     /
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.              /
+// See the License for the specific language governing permissions and                   /
+// limitations under the License.                                                        /
+//                                                                                       /
+// Written by João Vitor Verona Biazibetti <joaaoverona@gmail.com>, March 2016           /
+// https://www.github.com/BloodShura                                                     /
+//////////////////////////////////////////////////////////////////////////////////////////
+
+package br.shura.venus.origin;
+
+import br.shura.x.lang.IResource;
+
+import java.io.IOException;
+
+/**
+ * StreamScriptOrigin.java
+ *
+ * @author <a href="https://www.github.com/BloodShura">BloodShura</a> (João Vitor Verona Biazibetti)
+ * @contact joaaoverona@gmail.com
+ * @date 11/05/16 - 00:45
+ * @since GAMMA - 0x3
+ */
+public class StreamScriptOrigin implements ScriptOrigin {
+  private final String name;
+  private final IResource resource;
+
+  public StreamScriptOrigin(String name, IResource resource) {
+    this.name = name;
+    this.resource = resource;
+  }
+
+  @Override
+  public ScriptOrigin findInclude(String includeName) {
+    return null;
+  }
+
+  public IResource getResource() {
+    return resource;
+  }
+
+  @Override
+  public String getScriptName() {
+    return name;
+  }
+
+  @Override
+  public String read() throws IOException {
+    return getResource().readString();
+  }
+
+  @Override
+  public String toString() {
+    return "streamorigin(" + getScriptName() + ')';
+  }
+}
