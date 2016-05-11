5adec01ff0453b4d6382b56ec2f0135654f29393
BloodShura <joaaoverona@gmail.com>
2016-05-10 22:42:25 -0300

    Added standard library (StdLibrary), and 2 methods (Print and Println)

---
+++ src/br/shura/venus/library/std/Print.java
@@ -0,0 +1,49 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016, João Vitor Verona Biazibetti - All Rights Reserved                /
+//                                                                                       /
+// Licensed under the GNU General Public License v3;                                     /
+// you may not use this file except in compliance with the License.                      /
+//                                                                                       /
+// You may obtain a copy of the License at                                               /
+//     http://www.gnu.org/licenses/gpl.html                                              /
+//                                                                                       /
+// Unless required by applicable law or agreed to in writing, software                   /
+// distributed under the License is distributed on an "AS IS" BASIS,                     /
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.              /
+// See the License for the specific language governing permissions and                   /
+// limitations under the License.                                                        /
+//                                                                                       /
+// Written by João Vitor Verona Biazibetti <joaaoverona@gmail.com>, March 2016           /
+// https://www.github.com/BloodShura                                                     /
+//////////////////////////////////////////////////////////////////////////////////////////
+
+package br.shura.venus.library.std;
+
+import br.shura.venus.component.function.Method;
+import br.shura.venus.component.function.annotation.MethodName;
+import br.shura.venus.component.function.annotation.MethodVarArgs;
+import br.shura.venus.exception.ScriptRuntimeException;
+import br.shura.venus.executor.Context;
+import br.shura.venus.value.Value;
+import br.shura.x.logging.XLogger;
+
+/**
+ * Print.java
+ *
+ * @author <a href="https://www.github.com/BloodShura">BloodShura</a> (João Vitor Verona Biazibetti)
+ * @contact joaaoverona@gmail.com
+ * @date 09/05/16 - 20:24
+ * @since GAMMA - 0x3
+ */
+@MethodName("print")
+@MethodVarArgs
+public class Print extends Method {
+  @Override
+  public Value call(Context context, Value... arguments) throws ScriptRuntimeException {
+    for (Value argument : arguments) {
+      XLogger.print(argument);
+    }
+
+    return null;
+  }
+}
