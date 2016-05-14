cca9bbe892b8b0e56d0ec052d47ae2262cf341a6
BloodShura <joaaoverona@gmail.com>
2016-05-14 16:21:53 -0300

    Added Consume component (part of #6)

---
+++ src/br/shura/venus/component/sync/Consume.java
@@ -0,0 +1,48 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016, João Vitor Verona Biazibetti - All Rights Reserved                /
+//                                                                                       /
+// Licensed under the GNU General Public License v3;                                     /
+// you may not use this file except in compliance with the License.                      /
+//                                                                                       /
+// You may obtain a copy of the License at                                               /
+//     http://www.gnu.org/licenses/gpl.html                                              /
+//                                                                                       /
+// Unless required by applicable law or agreed to in writing, software                   /
+// distributed under the License is distributed on an "AS IS" BASIS,                     /
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.              /
+// See the License for the specific language governing permissions and                   /
+// limitations under the License.                                                        /
+//                                                                                       /
+// Written by João Vitor Verona Biazibetti <joaaoverona@gmail.com>, March 2016           /
+// https://www.github.com/BloodShura                                                     /
+//////////////////////////////////////////////////////////////////////////////////////////
+
+package br.shura.venus.component.sync;
+
+import br.shura.venus.component.Component;
+import br.shura.venus.resultor.Variable;
+
+/**
+ * Consume.java
+ *
+ * @author <a href="https://www.github.com/BloodShura">BloodShura</a> (João Vitor Verona Biazibetti)
+ * @contact joaaoverona@gmail.com
+ * @date 14/05/16 - 15:20
+ * @since GAMMA - 0x3
+ */
+public class Consume extends Component {
+  private final Variable variable;
+
+  public Consume(Variable variable) {
+    this.variable = variable;
+  }
+
+  public Variable getVariable() {
+    return variable;
+  }
+
+  @Override
+  public String toString() {
+    return "consume(" + getVariable() + ')';
+  }
+}
