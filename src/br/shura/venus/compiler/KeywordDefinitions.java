3e9d287b921343916fe9492f99e7f21eb92e6f70
BloodShura <joaaoverona@gmail.com>
2016-05-13 21:40:17 -0300

    Added KeywordDefinitions.CONTINUE

--- src/br/shura/venus/compiler/KeywordDefinitions.java
+++ src/br/shura/venus/compiler/KeywordDefinitions.java
@@ -33,6 +33,7 @@ import br.shura.x.worker.enumeration.Enumerations;
 public class KeywordDefinitions {
   public static final char COMMENTER = '#';
   public static final String BREAK = "break";
+  public static final String CONTINUE = "continue";
   public static final String DEFINE = "def";
   public static final String DO = "do";
   public static final String ELSE = "else";
