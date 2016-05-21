de63871d1639b3d46ba448bd4518fc6c019b1e7a
BloodShura <joaaoverona@gmail.com>
2016-05-21 01:18:19 -0300

    Added Evaluate method and added it to DynamicLibrary (it's just like interpret(), but it also returns any value which would be returned by the interpreted snippet, like function call results)

--- src/br/shura/venus/library/dynamic/Evaluate.java
+++ src/br/shura/venus/library/dynamic/Evaluate.java
@@ -19,18 +19,57 @@
 
 package br.shura.venus.library.dynamic;
 
-import br.shura.venus.library.VenusLibrary;
+import br.shura.venus.compiler.VenusLexer;
+import br.shura.venus.compiler.VenusParser;
+import br.shura.venus.component.SimpleContainer;
+import br.shura.venus.exception.ScriptCompileException;
+import br.shura.venus.exception.ScriptRuntimeException;
+import br.shura.venus.executor.Context;
+import br.shura.venus.function.FunctionCallDescriptor;
+import br.shura.venus.function.Method;
+import br.shura.venus.function.annotation.MethodName;
+import br.shura.venus.function.annotation.MethodVarArgs;
+import br.shura.venus.origin.SimpleScriptOrigin;
+import br.shura.venus.value.BoolValue;
+import br.shura.venus.value.Value;
+import br.shura.x.charset.build.TextBuilder;
+import br.shura.x.util.Pool;
+
+import java.io.IOException;
 
 /**
- * DynamicLibrary.java
+ * Evaluate.java
  *
  * @author <a href="https://www.github.com/BloodShura">BloodShura</a> (João Vitor Verona Biazibetti)
  * @contact joaaoverona@gmail.com
- * @date 14/05/16 - 00:47
+ * @date 21/05/16 - 00:45
  * @since GAMMA - 0x3
  */
-public class DynamicLibrary extends VenusLibrary {
-  public DynamicLibrary() {
-    addAll(DynamicInclude.class, DynamicUsing.class, Evaluate.class, HasFunction.class, Interpret.class);
+@MethodName("evaluate")
+@MethodVarArgs
+public class Evaluate extends Method {
+  @Override
+  public Value call(Context context, FunctionCallDescriptor descriptor) throws ScriptRuntimeException {
+    VenusParser parser = context.getOwner().getScript().getParser();
+    TextBuilder builder = Pool.newBuilder();
+
+    builder.appendln(descriptor.getValues());
+
+    String source = builder.toStringAndClear();
+    SimpleScriptOrigin origin = new SimpleScriptOrigin("Interpreted-Script", source);
+    SimpleContainer container = new SimpleContainer();
+
+    container.setParent(context.getOwner());
+
+    try {
+      parser.parse(new VenusLexer(origin), container);
+
+      Value result = context.currentExecutor().run(container);
+
+      return result != null ? result : new BoolValue(false);
+    }
+    catch (IOException | ScriptCompileException exception) {
+      throw new ScriptRuntimeException(context, "Failed to interpret script", exception);
+    }
   }
 }
