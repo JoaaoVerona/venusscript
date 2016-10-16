2f3253baa4b44aa1a2e21aa196dbb62a818bdca7
BloodShura <joaaoverona@gmail.com>
2016-10-15 23:49:04 -0300

    Updated copyright header

--- src/com/github/bloodshura/venus/exception/runtime/IncompatibleTypesException.java
+++ src/com/github/bloodshura/venus/exception/runtime/IncompatibleTypesException.java
@@ -1,19 +1,19 @@
 //////////////////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2016, João Vitor Verona Biazibetti - All Rights Reserved                /
 //                                                                                       /
-// Licensed under the GNU General Public License v3;                                     /
-// you may not use this file except in compliance with the License.                      /
+// This program is free software: you can redistribute it and/or modify                  /
+// it under the terms of the GNU General Public License as published by                  /
+// the Free Software Foundation, either version 3 of the License, or                     /
+// (at your option) any later version.                                                   /
 //                                                                                       /
-// You may obtain a copy of the License at                                               /
-//     http://www.gnu.org/licenses/gpl.html                                              /
+// This program is distributed in the hope that it will be useful,                       /
+// but WITHOUT ANY WARRANTY; without even the implied warranty of                        /
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                          /
+// GNU General Public License for more details.                                          /
 //                                                                                       /
-// Unless required by applicable law or agreed to in writing, software                   /
-// distributed under the License is distributed on an "AS IS" BASIS,                     /
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.              /
-// See the License for the specific language governing permissions and                   /
-// limitations under the License.                                                        /
+// You should have received a copy of the GNU General Public License                     /
+// along with this program. If not, see <http://www.gnu.org/licenses/>.                  /
 //                                                                                       /
-// Written by João Vitor Verona Biazibetti <joaaoverona@gmail.com>, March 2016           /
 // https://www.github.com/BloodShura                                                     /
 //////////////////////////////////////////////////////////////////////////////////////////
 
